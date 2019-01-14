2a2e39e3aabf3797ca796464d3537affd50d174b
82108 <leeyh2006@nate.com>
2019-01-15 00:54:39 +0900

    frontend Project

---
+++ src/components/member/memberForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Member extends Component{
+    render() {
+       return(
+           <div>
+                This is Member Page
+           </div>
+       )
+    }
+}
+
+
+export default Member
