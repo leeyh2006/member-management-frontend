9c9e34483258bf9220691a0f7dd35e64b6fa7f63
82108 <leeyh2006@nate.com>
2019-01-15 14:31:24 +0900

    action

---
+++ src/action/action.js
@@ -0,0 +1,19 @@
+/**
+ *  액션 타입
+ * @type {string}
+ */
+export const ADD_TODO ='ADD_TODO';
+export const COMPLTE_TODO = 'COMPLETE_TODO';
+export const SET_VISIBILITY_FILTER='SET_VISIBILITY_FILTER';
+
+/**
+ * 다른 상수
+ */
+
+export const VisibilityFilters ={
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE:'SHOW_ACTIVE'
+};
+
+
